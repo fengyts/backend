bca720e732eb068544e98b36b4c0727dae340f51
fengyts <fengyts@163.com>
2020-09-08 22:49:21 +0800

    feat: sysrole

--- backend-admin/src/main/resources/templates/system/sysRole/sysRoleMenu.js
+++ backend-admin/src/main/resources/templates/system/sysRole/sysRoleMenu.js
@@ -1,13 +1,13 @@
 $(function () {
-
     layui.extend({
         eleTree: '/plugins/layui-extend/eleTree'
     }).use(['eleTree'], function () {
         var eleTree = layui.eleTree;
-        eleTree.render({
+        var eleT = eleTree.render({
             elem: '.ele',
             defaultExpandAll: true,
             showCheckbox: true,
+            // showLine: true,
             data: [
                 {
                     "id": 1,
@@ -82,6 +82,28 @@ $(function () {
                 }
             ],
 
+        });
+        // 获取选中的值
+        $('.getChecked').click(function () {
+            var c = eleT.getChecked(false, true);
+        });
+        // 全选 /反选
+        $('.checkedAll').click(function () {
+            let $ele = $(".ele input:checkbox");
+            let checkedAllFlag = $("#checkedAllFlag").val();
+            if (0 == checkedAllFlag) {
+                $ele.attr('checked', true);
+                $ele.attr('eletree-status', '1');
+                $(".eleTree-checkbox").addClass('eleTree-checkbox-checked');
+                $(".layui-icon").addClass('layui-icon-ok');
+                $("#checkedAllFlag").val("1");
+            } else {
+                $ele.attr('checked', false);
+                $ele.attr('eletree-status', '0');
+                $(".eleTree-checkbox").removeClass('eleTree-checkbox-checked');
+                $(".layui-icon").removeClass('layui-icon-ok');
+                $("#checkedAllFlag").val("0");
+            }
         });
     });
 });
