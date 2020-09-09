78e125919357eede9ee31321e662664637f55746
fengyts <fengyts@163.com>
2020-09-09 23:12:18 +0800

    feat: sysrolemenu

--- backend-admin/src/main/resources/templates/system/sysRole/sysRoleMenu.js
+++ backend-admin/src/main/resources/templates/system/sysRole/sysRoleMenu.js
@@ -3,106 +3,124 @@ $(function () {
         eleTree: '/plugins/layui-extend/eleTree'
     }).use(['eleTree'], function () {
         var eleTree = layui.eleTree;
-        var eleT = eleTree.render({
-            elem: '.ele',
-            defaultExpandAll: true,
-            showCheckbox: true,
-            // showLine: true,
-            data: [
-                {
-                    "id": 1,
-                    "label": "安徽省",
-                    "children": [
-                        {
-                            "id": 2,
-                            "label": "马鞍山市",
-                            "disabled": true,
-                            "children": [
-                                {
-                                    "id": 3,
-                                    "label": "和县"
-                                },
-                                {
-                                    "id": 4,
-                                    "label": "花山区",
-                                    "checked": true
-                                }
-                            ]
-                        },
-                        {
-                            "id": 22,
-                            "label": "淮北市",
-                            "children": [
-                                {
-                                    "id": 23,
-                                    "label": "濉溪县"
-                                },
-                                {
-                                    "id": 24,
-                                    "label": "相山区",
-                                    "checked": true
-                                }
-                            ]
-                        }
-                    ]
-                },
-                {
-                    "id": 5,
-                    "label": "河南省",
-                    "children": [
-                        {
-                            "id": 6,
-                            "label": "郑州市"
-                        }
-                    ]
-                },
-                {
-                    "id": 10,
-                    "label": "江苏省",
-                    "children": [
-                        {
-                            "id": 11,
-                            "label": "苏州市"
-                        },
-                        {
-                            "id": 12,
-                            "label": "南京市",
-                            "children": [
-                                {
-                                    "id": 13,
-                                    "label": "姑苏区"
-                                },
-                                {
-                                    "id": 14,
-                                    "label": "相城区"
-                                }
-                            ]
-                        }
-                    ]
-                }
-            ],
+        var data = [
+            {
+                "label": "安徽省",
+                "id": "001",
+                "isOpen": true,
+                "children": [
+                    {
+                        "label": "淮北市",
+                        "id": "001001"
+                    },
+                    {
+                        "label": "马鞍山市",
+                        "id": "001002",
+                        "isOpen": true,
+                        "children": [
+                            {
+                                "label": "花山区",
+                                "id": "001002001",
+                                "isOpen": true,
+                                "children": [
+                                    {
+                                        "label": "霍里街道",
+                                        "disabled": true,
+                                        "checked": true,
+                                        "id": "001002002001",
+                                        "radioDisabled": true
+                                    },
+                                    {
+                                        "label": "桃源路",
+                                        "id": "001002002002"
+                                    },
+                                    {
+                                        "label": "湖东路",
+                                        "checked": true,
+                                        "id": "001002002003",
+                                        "radioChecked": true
+                                    }
+                                ]
+                            },
+                            {
+                                "label": "雨山区",
+                                "id": "001002002"
+                            },
+                            {
+                                "label": "和县",
+                                "id": "001002003"
+                            }
+                        ]
+                    },
+                    {
+                        "label": "合肥市",
+                        "id": "001003"
+                    }
+                ]
+            },
+            {
+                "label": "河南省",
+                "id": "002",
+                "children": [
+                    {
+                        "label": "郑州市",
+                        "id": "002001"
+                    },
+                    {
+                        "label": "开封市",
+                        "id": "002002"
+                    }
+                ]
+            },
+            {
+                "label": "江苏省",
+                "id": "003",
+                "children": [
+                    {
+                        "label": "苏州市",
+                        "id": "003001"
+                    },
+                    {
+                        "label": "南京市",
+                        "id": "003002"
+                    },
+                    {
+                        "label": "无锡市",
+                        "id": "003003"
+                    },
+                    {
+                        "label": "徐州市",
+                        "id": "003004"
+                    }
+                ]
+            }
+        ]
 
-        });
+        var eleT = eleTree({
+            el: '.eleTree',
+            data: data,
+            showCheckbox: true,
+            highlightCurrent: true,
+        })
+        eleT.on('checkbox', function (data) {
+            // console.log(this)
+            console.log(data)
+        })
         // 获取选中的值
         $('.getChecked').click(function () {
             var c = eleT.getChecked(false, true);
+            console.log(c);
         });
-        // 全选 /反选
-        $('.checkedAll').click(function () {
-            let $ele = $(".ele input:checkbox");
-            let checkedAllFlag = $("#checkedAllFlag").val();
-            if (0 == checkedAllFlag) {
-                $ele.attr('checked', true);
-                $ele.attr('eletree-status', '1');
-                $(".eleTree-checkbox").addClass('eleTree-checkbox-checked');
-                $(".layui-icon").addClass('layui-icon-ok');
-                $("#checkedAllFlag").val("1");
+        // 全选/反选
+        $(".checkedAll").click(function () {
+            eleT.setAllChecked();
+            let _ca = $("#checkedAllFlag").val();
+            if ('0' === _ca) {
+                eleT.setAllChecked();
+                $("#checkedAllFlag").val('1');
             } else {
-                $ele.attr('checked', false);
-                $ele.attr('eletree-status', '0');
-                $(".eleTree-checkbox").removeClass('eleTree-checkbox-checked');
-                $(".layui-icon").removeClass('layui-icon-ok');
-                $("#checkedAllFlag").val("0");
+                eleT.reverseChecked();
+                $("#checkedAllFlag").val('0');
             }
         });
     });
