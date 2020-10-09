22c81d5420ed5ae5f0a0cf1d6f69ef6a592cd317
fengyt <fengyuntao@e-lead.cn>
2020-10-09 09:37:32 +0800

    fixed:code merge bug

--- backend-admin/src/main/java/com/backend/system/controller/SysRoleController.java
+++ backend-admin/src/main/java/com/backend/system/controller/SysRoleController.java
@@ -59,7 +59,6 @@ public class SysRoleController extends BaseController {
     public ResultData getAllMenus(Long roleId) {
         List<SysRoleMenuDto> roleMenus = sysRoleService.getSysRoleMenus(roleId);
         ResultData res = ResultData.ok(roleMenus);
-        System.out.println(JSONObject.toJSONString(res));
         return res;
 
 //        String data = "[{\"label\":\"安徽省\",\"id\":\"001\",\"isOpen\":true,\"children\":[{\"label\":\"淮北市\",\"id\":\"001001\"},{\"label\":\"马鞍山市\",\"id\":\"001002\",\"isOpen\":true,\"children\":[{\"label\":\"花山区\",\"id\":\"001002001\",\"isOpen\":true,\"children\":[{\"label\":\"霍里街道\",\"disabled\":true,\"checked\":true,\"id\":\"001002002001\",\"radioDisabled\":true},{\"label\":\"桃源路\",\"id\":\"001002002002\"},{\"label\":\"湖东路\",\"checked\":true,\"id\":\"001002002003\",\"radioChecked\":true}]},{\"label\":\"雨山区\",\"id\":\"001002002\"},{\"label\":\"和县\",\"id\":\"001002003\"}]},{\"label\":\"合肥市\",\"id\":\"001003\"}]},{\"label\":\"河南省\",\"id\":\"002\",\"children\":[{\"label\":\"郑州市\",\"id\":\"002001\"},{\"label\":\"开封市\",\"id\":\"002002\"}]},{\"label\":\"江苏省\",\"id\":\"003\",\"children\":[{\"label\":\"苏州市\",\"id\":\"003001\"},{\"label\":\"南京市\",\"id\":\"003002\"},{\"label\":\"无锡市\",\"id\":\"003003\"},{\"label\":\"徐州市\",\"id\":\"003004\"}]}]";
