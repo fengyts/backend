7c6f251991c6828be5cf302754a0a17bac8b1ddb
fengyts <fengyts@163.com>
2020-10-19 21:43:35 +0800

    feat: resolve conflict

--- backend-flowable/src/main/java/com/backend/controller/sys/IndexController.java
+++ backend-flowable/src/main/java/com/backend/controller/sys/IndexController.java
@@ -13,13 +13,15 @@ public class IndexController extends BaseController {
 
     /**
      * 因为集成了flowable-modeler-ui, 所以把"/"路径留出来用以跳转到 flowable-modeler-ui
+     * 首页不能命名为 index, 因为index和flowable-modler-ui的 index冲突
      * @param
      * @return
      */
 //    @RequestMapping({"/","index"})
     @RequestMapping({"/index"})
     public String index(){
-        return "/index";
+//        return "/index";
+        return "/home";
     }
 
 }
